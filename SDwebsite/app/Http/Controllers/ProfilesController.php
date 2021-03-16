094b54573ffbe7c28f3f10cc77f285a0c851990c
AhmadAFS1 <59811140+AhmadAFS1@users.noreply.github.com>
2021-03-16 13:18:59 -0500

    added  edit profile tab to dropdown menu

--- SDwebsite/app/Http/Controllers/ProfilesController.php
+++ SDwebsite/app/Http/Controllers/ProfilesController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Request;
+use App\Models\profile;
 
 class ProfilesController extends Controller
 {
-    //
-    //SCREW THIS 
+    function show()
+    {
+   //   $data = QuoteHistory::all();
+   //   return view('fuelquotehistory', 'orders' => $data);   uncomment once DB is implemented
+      
+      return view('profile');
+      
+    }
 }
