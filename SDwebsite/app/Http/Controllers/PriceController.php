d454df64c128967b6f814bc938fdfe70af1eb9f7
AhmadAFS1 <59811140+AhmadAFS1@users.noreply.github.com>
2021-04-21 01:01:31 -0500

    added plan for Price Controller

--- SDwebsite/app/Http/Controllers/PriceController.php
+++ SDwebsite/app/Http/Controllers/PriceController.php
@@ -24,13 +24,43 @@ class PriceController extends Controller
             $fulladdress = "No address given!\nUsing Texas Price ($2) as default";
         }
 
+        //Final Assignment thing 
+        /*
+        function getTheActualPrice(){
+        
+        int basePay = $1.50; //given
+        
+        if(location not in Texas){
+            int locationFactor = 0.04;
+        }else{int locationFactor = 0.02;} //self-explanatory 
+        
+        //find whether the user's id is even in the  FuelQuoteHistory table at all
+
+        if (DB::table('QuoteHistory')->where('user_id', $user)->exists()) {
+            int HistoryFactor = 0.01;
+        }
+        if (DB::table('QuoteHistory')->where('user_id', $user)->doesntExist()) {
+            int HistoryFactor = 0;
+        }
+
+        if(Gallons > 1000)  <- idk how to do this one in this controller
+        int GallonsRequestedFactor = idk; 
+
+
+        int CompanyProfit = 0.10;
+        
+        int ResultingPrice = (locationFactor - HistoryFactor + GallonsRequestedFactor)*basePay
+        }
+        */
+
+
         if(empty($address) || empty($city) || empty($state) || empty($zip))
         {
             $fulladdress = "Full address not given!"; 
             $Suggested_Price = 3.0;
         }
         else
-        {
+        {   
             $t_state = $state;
 
             if($t_state != 'TX' or $t_state == NULL){
