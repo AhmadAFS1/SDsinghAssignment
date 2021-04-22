a838166d4a60902d8c9bde4b022306893e2fa271
AhmadAFS1 <59811140+AhmadAFS1@users.noreply.github.com>
2021-04-21 23:50:38 -0500

    commit before pull

--- SDwebsite/app/Http/Controllers/PriceController.php
+++ SDwebsite/app/Http/Controllers/PriceController.php
@@ -24,14 +24,17 @@ class PriceController extends Controller
             $fulladdress = "No address given!\nUsing Texas Price ($2) as default";
         }
 
-        //Final Assignment thing 
-        /*
+        
         function getTheActualPrice(){
         
+       
+
+
         int basePay = $1.50; //given
-        
-        if(location not in Texas){
-            int locationFactor = 0.04;
+
+        $t_state = $state;
+        if($t_state != 'TX' or $t_state == NULL){
+                $t_state = 'Others';
         }else{int locationFactor = 0.02;} //self-explanatory 
         
         //find whether the user's id is even in the  FuelQuoteHistory table at all
@@ -43,8 +46,8 @@ class PriceController extends Controller
             int HistoryFactor = 0;
         }
 
-        if(Gallons > 1000)  <- idk how to do this one in this controller
-        int GallonsRequestedFactor = idk; 
+        //if(Gallons > 1000)  <- idk how to do this one in this controller
+        int GallonsRequestedFactor = 0; 
 
 
         int CompanyProfit = 0.10;
@@ -53,7 +56,7 @@ class PriceController extends Controller
 
         return ResultingPrice;
         }
-        */
+        
 
 
         if(empty($address) || empty($city) || empty($state) || empty($zip))
@@ -63,6 +66,7 @@ class PriceController extends Controller
         }
         else
         {   
+            /*
             $t_state = $state;
 
             if($t_state != 'TX' or $t_state == NULL){
@@ -76,7 +80,7 @@ class PriceController extends Controller
             else{
                 Controller::console_log('No data in prices table!');
                 $Suggested_Price = 3.0;
-            }
+            }*/
         }
 
         $Address = $fulladdress;        
