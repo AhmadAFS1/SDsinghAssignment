40d3db97df9ddd2f4215cf8d8c71564822ca6512
Nathan N <nhatnguyen2807@gmail.com>
2021-04-25 15:41:50 -0500

    pull

--- SDwebsite/resources/views/fuelquoteform.blade.php
+++ SDwebsite/resources/views/fuelquoteform.blade.php
@@ -92,7 +92,7 @@
 
                         
                         @auth
-                        <div class="form-group row mb-0">
+                        <div class="form-group row mb-0" oninput="">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('SUBMIT') }}
